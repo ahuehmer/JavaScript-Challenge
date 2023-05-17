c0914f3e55ffe50ea24d7edd474af6c61accb2b5
Anja Huhmer <ahuehmer@gmail.com>
2023-05-17 01:01:01 -0700

    Starting

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Create URL variable
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+// Read in JSON file
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
